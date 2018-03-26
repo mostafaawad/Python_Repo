9ca39ab8b80dd8901ed1e7042d5f7db0194d89d7
mostafaawad <mostafaawad995@gmail.com>
2018-03-26 15:50:39 +0200

    finalizing inheritance and method overriding

--- inheritance.py
+++ inheritance.py
@@ -1,18 +1,30 @@
 class parent:
-	def __init__(self,lastName,eyeColor):
-		print "parent constructor called"
-		self.lastName = lastName
-		self.eyeColor = eyeColor
+    def __init__(self, lastName, eyeColor):
+        print "parent constructor called"
+        self.lastName = lastName
+        self.eyeColor = eyeColor
+
+    def show_info(self):
+        print "Lastname is: " + self.lastName
+        print "Eye color is: " + self.eyeColor
 
 class child(parent):
-	def __init__(self,lastName,eyeColor,toysNumber):
-		print "child constructor called"
-		parent.__init__(self,lastName,eyeColor)
-		self.toysNumber = toysNumber
+    def __init__(self, lastName, eyeColor, toysNumber):
+        print "child constructor called"
+        parent.__init__(self, lastName, eyeColor)
+        self.toysNumber = toysNumber
 
-dad = parent("cyrus","blue")
-print dad.lastName
+    def show_info(self):
+        print "Lastname is: " + self.lastName
+        print "Eye color is: " + self.eyeColor
+        print "Number of toys are: " + str(self.toysNumber)
 
-daughter = child("cyrus","blue",5)
-print daughter.lastName
-print daughter.toysNumber
+dad = parent("cyrus", "blue")
+#print dad.lastName
+
+#dad.show_info()
+
+daughter = child("cyrus", "blue", 5)
+daughter.show_info()
+#print daughter.lastName
+#print daughter.toysNumber
