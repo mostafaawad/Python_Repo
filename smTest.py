3b156287255e22325ea1292e61a077016eb6f155
mostafaawad <mostafaawad995@gmail.com>
2018-03-28 11:46:54 +0200

    MCP mp3 to json extractor

---
+++ smTest.py
@@ -0,0 +1,19 @@
+import os, glob, io, json
+
+try:
+    to_unicode = unicode
+except NameError:
+    to_unicode = str
+
+files = []
+
+os.chdir("mp3/")
+for file in glob.glob("*.mp3"):
+    files.append(file[0:32])
+
+jsonData = {"externalId" : files}
+
+with io.open('data.json', 'w', encoding='utf8') as outfile:
+    str_ = json.dumps(jsonData,indent=4,sort_keys=True,separators=(',', ': '), ensure_ascii=False)
+    outfile.write(to_unicode(str_))
+print str_
