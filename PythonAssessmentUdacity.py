d5514c543aafb9f678313f2c99cea9d2dd9fc87b
Mostafa Awad <mostafaawad995@gmail.com>
2018-04-03 22:59:03 +0200

    Adding assessment answers

---
+++ PythonAssessmentUdacity.py
@@ -0,0 +1,56 @@
+# new = ""
+# for i in "Hello":
+# 	if i != "l":
+# 		new += i
+# print new
+
+# s = "Hello World"
+# print s[:5]
+
+# def sum(a,b):
+# 	a = a+b
+# print sum(5,4)
+
+# x = 1
+# def mf():
+# 	x = 5
+# mf()
+# print x
+
+# x = [2,1,3,4]
+# print x[1:]
+
+# import math
+# print "ab".upper()
+# print not(5>3)
+# print math.floor(4.6)
+
+# s = "any string"
+# print s.find(s)
+# print 's'.find('s')
+# print s.find('')
+# print 2*50+100
+
+# a = "Udacity"
+# b = 5
+# print a+b
+
+# li = [""hi"",""welcome""]
+
+# print "1" == 1
+
+# a = [5,959,665,6326,56,2,4,90]
+# a.sort()
+# print a 
+
+# print "udacity"*2
+
+# for a in range(2,5):
+# 	print (a+(a-1))
+
+# arr = [[5,4,2],[4,4,1],[8,9,0],3]
+# print arr[0][0] + arr[-1]
+
+# print '\\ I\'m \"Awesome\"'
+_=3
+print _
